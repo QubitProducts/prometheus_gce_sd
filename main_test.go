8e122dfcc10f33ef9b51c3ba70ce9f9603cba32d
Laurie Clark-Michalek <laurie@qubit.com>
2016-08-23 15:59:17 +0100

    Add test for InstanceToTarget

--- main_test.go
+++ main_test.go
@@ -6,6 +6,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"reflect"
+
+	compute "google.golang.org/api/compute/v1"
 )
 
 func TestLoadConfigFile(t *testing.T) {
@@ -43,6 +45,107 @@ func TestLoadConfigFile(t *testing.T) {
 			t.Parallel()
 
 			res, err := LoadConfigFile(c.path)
+			if c.expectedError {
+				if err == nil {
+					t.Fatalf("Unexpected success\nResult: %v", prettyPrint(res))
+				}
+			} else {
+				if err != nil {
+					t.Fatalf("Unexpected error\nError: %v", err)
+				}
+
+				if reflect.DeepEqual(res, c.expected) {
+					t.Fatalf("Discrepancy in result\nResult: %v", prettyPrint(res))
+				}
+			}
+		})
+	}
+}
+
+func TestInstanceToTarget(t *testing.T) {
+	t.Parallel()
+
+	cases := []struct {
+		instance      *compute.Instance
+		config        SearchConfig
+		expected      DiscoveryTarget
+		expectedError bool
+	}{
+		{
+			instance: &compute.Instance{
+				Zone:        "https://www.googleapis.com/compute/v1/projects/qubit-vcloud-us-proc-stg/zones/us-central1-b",
+				MachineType: "https://www.googleapis.com/compute/v1/projects/qubit-vcloud-us-proc-stg/zones/us-central1-b/machineTypes/g1-small",
+				Tags: &compute.Tags{
+					Items: []string{"foo"},
+				},
+				NetworkInterfaces: []*compute.NetworkInterface{
+					{NetworkIP: "127.0.0.1"},
+				},
+			},
+			config: SearchConfig{
+				Ports:   []int{8080, 9090},
+				Project: "test-project",
+			},
+			expected: DiscoveryTarget{
+				Targets: []string{"127.0.0.1:8080", "127.0.0.1:9090"},
+				Labels: map[string]string{
+					"gce_instance_tag_foo": "true",
+					"gce_instance_zone":    "us-central-1b",
+					"gce_instance_type":    "g1-small",
+					"gce_instance_project": "us-central-1b",
+				},
+			},
+			expectedError: false,
+		},
+		{ // No network interfaces
+			instance: &compute.Instance{
+				Zone:        "https://www.googleapis.com/compute/v1/projects/qubit-vcloud-us-proc-stg/zones/us-central1-b",
+				MachineType: "https://www.googleapis.com/compute/v1/projects/qubit-vcloud-us-proc-stg/zones/us-central1-b/machineTypes/g1-small",
+				Tags: &compute.Tags{
+					Items: []string{"foo"},
+				},
+				NetworkInterfaces: []*compute.NetworkInterface{},
+			},
+			config: SearchConfig{
+				Ports:   []int{8080, 9090},
+				Project: "test-project",
+			},
+			expectedError: true,
+		},
+		{ // Tags to lower and - to _
+			instance: &compute.Instance{
+				Zone:        "https://www.googleapis.com/compute/v1/projects/qubit-vcloud-us-proc-stg/zones/us-central1-b",
+				MachineType: "https://www.googleapis.com/compute/v1/projects/qubit-vcloud-us-proc-stg/zones/us-central1-b/machineTypes/g1-small",
+				Tags: &compute.Tags{
+					Items: []string{"FOO-BAR"},
+				},
+				NetworkInterfaces: []*compute.NetworkInterface{
+					{NetworkIP: "127.0.0.1"},
+				},
+			},
+			config: SearchConfig{
+				Ports:   []int{8080, 9090},
+				Project: "test-project",
+			},
+			expected: DiscoveryTarget{
+				Targets: []string{"127.0.0.1:8080", "127.0.0.1:9090"},
+				Labels: map[string]string{
+					"gce_instance_tag_foo_bar": "true",
+					"gce_instance_zone":        "us-central-1b",
+					"gce_instance_type":        "g1-small",
+					"gce_instance_project":     "us-central-1b",
+				},
+			},
+			expectedError: false,
+		},
+	}
+
+	for _, c := range cases {
+		c := c
+		t.Run("", func(t *testing.T) {
+			t.Parallel()
+
+			res, err := InstanceToTarget(c.instance, c.config)
 			if c.expectedError {
 				if err == nil {
 					t.Fatalf("Unexpected success\nResult: %v", prettyPrint(res))
